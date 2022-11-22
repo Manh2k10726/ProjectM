3b2c15a8757f5a8eb7276323c8e0bb007abbb467
Nguyen Manh <nguyenmanh26072k1@gmail.com>
2022-11-22 19:06:13 +0700

    update haeder-api clinic

--- src/controllers/clinicController.js
+++ src/controllers/clinicController.js
@@ -12,17 +12,17 @@ let createClinic = async(req,res)=>{
         })
      }
 }
-// let getAllClinic=async(req,res)=>{
-//    try {
-//       let info = await ClinicServices.getAllClinic();
-//         return res.status(200).json(info);
-//    } catch (e) {
-//       return res.status(200).json({
-//          errCode:-1,
-//          errMessage:'Error from the sever'
-//       })
-//    }
-// }
+let getAllClinic=async(req,res)=>{
+   try {
+      let info = await clinicServices.getAllClinic();
+        return res.status(200).json(info);
+   } catch (e) {
+      return res.status(200).json({
+         errCode:-1,
+         errMessage:'Error from the sever'
+      })
+   }
+}
 // let getDetailClinic=async(req,res)=>{
 //    try {
 //       let info = await ClinicServices.getDetailClinic(req.query.id,req.query.location);
@@ -35,5 +35,6 @@ let createClinic = async(req,res)=>{
 //    }
 // }
 module.exports = {
-    createClinic:createClinic
+    createClinic:createClinic,
+    getAllClinic:getAllClinic
 }
--- src/route/web.js
+++ src/route/web.js
@@ -44,7 +44,7 @@ let initWebRoutes = (app) => {
     router.get('/api/get-detail-specialty-by-id',specialtyController.getDetailSpecialty)
     
     router.post('/api/create-new-clinic',clinicController.createClinic)
-    // router.get('/api/get-all-specialty',specialtyController.getAllSpecialty)
+    router.get('/api/get-all-clinic',clinicController.getAllClinic)
     // router.get('/api/get-detail-specialty-by-id',specialtyController.getDetailSpecialty)
     
     return app.use("/", router);
